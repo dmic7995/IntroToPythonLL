d78edc24ecc934a17dd753f6ef4f641c9c63b119
Daniel Michael <daniel.michael071@gmail.com>
2021-09-15 16:56:17 +0400

    Loops

--- Ch2 Loops.py
+++ Ch2 Loops.py
@@ -6,8 +6,24 @@ def main():
     x = 0
 
 # Define a while-loop
-    while (x<5):
-        print(x)
-        x=x+1
+#    while (x<5):
+#        print(x)
+#        x=x+1
 
 # Define a for-loop
+#    for x in range(5, 10): #These for-loops are iterators - the range function gives you that range of numbers
+#        print(x)
+
+    #This range function is the same as for(i=5, i<10; i++) -> The 'i' count stops at 9
+# Use a for-loop over a collection
+#    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+#    for d in days:
+#        print(d)
+
+# Use the break and continue statements
+for x in range (5, 10):
+    if (x==7): break
+    print(x)
+
+ if __name__ == "__main__":
+     main()
